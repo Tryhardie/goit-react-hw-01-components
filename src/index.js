a520291d04e4ef2c31a4f615990e6ad59a9fcaa6
Tryhardie <realkalin@gmail.com>
2023-03-09 17:36:02 +0200

    Profile added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+// import { App } from 'components/App';
+import { Profile } from 'components/profile/Profile';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Profile />
   </React.StrictMode>
 );

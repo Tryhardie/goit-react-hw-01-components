b3d63907acf105b660caeecf7b01acf0e83afc40
Tryhardie <realkalin@gmail.com>
2023-05-17 15:04:51 +0300

    First task complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,24 +3,12 @@ import user from '../components/profile/user.json';
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-    </div>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
   );
 };
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,30 +1,71 @@
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 500px;
+  margin: 0 auto;
+  font-size: 24px;
+  background-color: #e6edf5;
+  outline: 1px solid #c1cfdf;
+`;
+
+const UserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 30px;
+`;
+
+const UserImage = styled.img`
+  border-radius: 50%;
+  max-height: 250px;
+`;
+
+const UserName = styled.p`
+  font-weight: 700;
+`;
+
+const UserStats = styled.ul`
+  display: flex;
+  justify-content: space-evenly;
+  background-color: #dbe5f0;
+  outline: 1px solid #c1cfdf;
+`;
+
+const UserStatItem = styled.li`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Quantity = styled.span`
+  font-weight: 500;
+`;
 
 export default function Profile({ username, tag, location, avatar, stats }) {
   return (
-    <div className="profile">
-      <div className="description">
-        <img src={avatar} alt="User avatar" className="avatar" />
-        <p className="name">{username}</p>
+    <Wrapper>
+      <UserInfo>
+        <UserImage src={avatar} alt="User avatar" />
+        <UserName>{username}</UserName>
         <p className="tag">@{tag}</p>
         <p className="location">{location}</p>
-      </div>
-
-      <ul className="stats">
-        <li>
+      </UserInfo>
+      <UserStats>
+        <UserStatItem>
           <span className="label">Followers</span>
-          <span className="quantity">{stats.followers}</span>
-        </li>
-        <li>
+          <Quantity>{stats.followers}</Quantity>
+        </UserStatItem>
+        <UserStatItem>
           <span className="label">Views</span>
-          <span className="quantity">{stats.views}</span>
-        </li>
-        <li>
+          <Quantity>{stats.views}</Quantity>
+        </UserStatItem>
+        <UserStatItem>
           <span className="label">Likes</span>
-          <span className="quantity">{stats.likes}</span>
-        </li>
-      </ul>
-    </div>
+          <Quantity>{stats.likes}</Quantity>
+        </UserStatItem>
+      </UserStats>
+    </Wrapper>
   );
 }
 
